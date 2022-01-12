d6f170dc6259638ad2c853888ab2e1158cdd7f47
AamirSoneji <sonejiaamir@gmail.com>
2022-01-12 16:02:21 +0530

    editied

--- hehe.js
+++ hehe.js
@@ -1,8 +1,4 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Data from './components/Data';
-import Body from './components/Body'
+
 
 function App(){
     return(
